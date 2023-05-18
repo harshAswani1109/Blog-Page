838cd4852bb2908d7aabdc7b4e96ce3fe35638f5
ha1109@srmist.edu.in <ha1109@srmist.edu.in>
2023-05-18 14:16:17 +0530

    minor changes

--- src/pages/blog/writeBlog.js
+++ src/pages/blog/writeBlog.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import { convertToRaw } from "draft-js";
@@ -6,32 +6,28 @@ import { convertToRaw } from "draft-js";
 import UserNavbar from "../../../components/userNavbar/navbar";
 
 const BlogEditor = () => {
-  const [editorState, setEditorState] = React.useState();
-  const [blogData, setBlogData] = React.useState({});
+  const [editorState, setEditorState] = useState(null);
+  const [blogData, setBlogData] = useState({});
   console.log(blogData);
 
-  // async function handleSubmit() {
-  //   const response = await submitForm({
-  //     name: formData.get("blogname"),
-  //     image: formData.get("image"),
-  //     content: formData.get("content"),
-  //   });
-  // }
-
-  // async function submitImage() {
-  //   "use server";
-  //   // ...
-  // }
+  const handleEditorStateChange = (editorState) => {
+    setEditorState(editorState);
+    setBlogData({
+      ...blogData,
+      body: editorState.getCurrentContent().getPlainText(),
+      raw: convertToRaw(editorState.getCurrentContent()),
+    });
+    console.log(convertToRaw(editorState.getCurrentContent()));
+    console.log(editorState.getCurrentContent().getPlainText());
+  };
 
   return (
     <div>
       <UserNavbar />
-      <form
-      // action={handleSubmit}
-      >
+      <form>
         <div className="flex flex-col justify-center items-center mx-[2%] my-10 gap-4 font-semibold">
           <div className="flex justify-start items-center border-2 w-full text-md md:text-xl p-2">
-            <label>Blog Name : </label>
+            <label>Blog Name: </label>
             <input className="outline-none px-2" type="text" name="name" />
           </div>
           <div className="flex justify-start items-center border-2 w-full text-md md:text-xl p-2 gap-4">
@@ -41,7 +37,6 @@ const BlogEditor = () => {
             <input
               className="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
               type="file"
-              // formAction={submitImage}
             />
           </div>
         </div>
@@ -51,16 +46,7 @@ const BlogEditor = () => {
             toolbarClassName="toolbarClassName"
             wrapperClassName="wrapperClassName"
             editorClassName="editorClassName"
-            onEditorStateChange={(e) => {
-              setEditorState(e);
-              setBlogData({
-                ...blogData,
-                body: e.getCurrentContent().getPlainText(),
-                raw: convertToRaw(e.getCurrentContent()),
-              });
-              console.log(convertToRaw(e.getCurrentContent()));
-              console.log(e.getCurrentContent().getPlainText());
-            }}
+            onEditorStateChange={handleEditorStateChange}
           />
         </div>
       </form>
